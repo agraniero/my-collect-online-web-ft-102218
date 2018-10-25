a567fd83dc316eaf08f012573c818d7fefa35692
Anthony Graniero <ananfro@gmail.com>
2018-10-25 18:02:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,11 +4,10 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-    yield array[i]
-    array[i].upcase
-    collection << array
+    yield array[i].upcase
+    
     i += 1
   end
-  collection
+
 end
 
